b239f291334c2020883d2ff0e84c209237604892
psandip223 <psandip223@gmail.com>
2023-04-27 18:41:52 -0400

    Print gender

---
+++ PrintGender/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("----- Print Gender -----");
+
+char gen;
+
+//Reading gender from user
+Console.WriteLine("Enter gender (M/F): ");
+gen = Convert.ToChar(Console.ReadLine());
+gen = char.ToUpper(gen);
+
+if (gen == 'M')
+{
+    Console.WriteLine("Gender is Male.");
+}
+else if (gen =='F')
+{
+    Console.WriteLine("Gender is Female.");
+}
+else
+{
+    Console.WriteLine("Invalid input.");
+}
